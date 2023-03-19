5e3013ae21b44d5c34fa8c04acbf968b456ca837
Nguyễn Bính <119107135+moclan01@users.noreply.github.com>
2023-03-19 21:31:43 +0700

    Update Pacman.java

--- src/view/Pacman.java
+++ src/view/Pacman.java
@@ -3,9 +3,7 @@ package view;
 import javax.swing.JFrame;
 
 import model.Model;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+
 
 public class Pacman extends JFrame {
 
@@ -21,37 +19,7 @@ public class Pacman extends JFrame {
 		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pac.setLocationRelativeTo(null);
 		
-		JMenuBar menubar;
-		JMenu game;
-		JMenu about;
-		JMenuItem new_game;
-		JMenuItem setting;
-		JMenuItem exit;
-		JMenuItem about_us;
 		
-		menubar = new JMenuBar();
-		
-		game = new JMenu("Game");
-		about = new JMenu("About");
-		
-		new_game = new JMenuItem("New game");
-		setting = new JMenuItem("Setting");
-		exit = new JMenuItem("Exit");
-		game = new JMenu("Game");
-		about_us = new JMenuItem("About us");
-		
-		pac.setJMenuBar(menubar);
-		
-		menubar.add(game);
-		menubar.add(about);
-		
-		game.add(new_game);
-		game.add(setting);
-		game.add(exit);
-		about.add(about_us);
-		
-		pac.setVisible(true);
-
 	}
 
 }
